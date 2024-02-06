a5eaeb9c0e84e1bdc62c53099b99e82bce6d9871
irfan701 <irfanhossain701@gmail.com>
2024-02-06 17:42:03 +0400

    created visitors ip

--- app/Http/Controllers/VisitorController.php
+++ app/Http/Controllers/VisitorController.php
@@ -7,5 +7,18 @@ use Illuminate\Http\Request;
 
 class VisitorController extends Controller
 {
+    function getVisitorDetails()
+    {
+        $ip_address=$_SERVER['REMOTE_ADDR'];
+        date_default_timezone_set("Asia/Dhaka");
+        $visit_time=date("h:i:sa");
+        $visit_date=date("d-m-Y");
 
+        $result=Visitor::insert([
+            'ip_address'=>$ip_address,
+            'visit_time'=>$visit_time,
+            'visit_date'=>$visit_date
+        ]);
+        return response()->json(['result' => $result]);
+    }
 }
